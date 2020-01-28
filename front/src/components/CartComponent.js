96bfd548fdc2098c6ce7c9f9b3a3d7366485310f
nimrik <drobinskyi@evergreens.in.ua>
2020-01-28 16:57:18 +0200

    front

---
+++ front/src/components/CartComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import ocean from "../../static/images/ocean-calm.jpg"
+// import sea from "../../static/images/sea.jpg"
+// import seaRocks from "../../static/images/sea-rocks.jpg"
+
+const Cart = props => (
+    <li className="box-container">
+        {/*{props.index === 0 && <img src={sea} alt=""/>}*/}
+        {/*{props.index === 1 && <img src={ocean} alt=""/>}*/}
+        {/*{props.index === 2 && <img src={seaRocks} alt=""/>}*/}
+    </li>
+);
+
+export default Cart;
