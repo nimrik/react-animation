b1d6e60c3b7798e95f100bca205b30f64c564989
nimrik <drobinskyi@evergreens.in.ua>
2020-01-28 19:56:10 +0200

    fix

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://nimrik:8454fd2e@node-cluster-vabgd.mongodb.net/stores?retryWrites=true&w=majority', {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useCreateIndex: true,
             useFindAndModify: false,
--- front/src/components/FormComponent.js
+++ front/src/components/FormComponent.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const Form = props => (
     <form className="form-field" onSubmit={props.userMethod}>
-        <label htmlFor="js-id">input</label>
-        <input type="text" id="js-id" name="textData"/>
+        <div className="form-field">
+            <label htmlFor="js-id">input</label>
+            <input type="text" id="js-id" name="textData"/>
+        </div>
+
+        <div className="form-field">
+            <label htmlFor=""></label>
+            <input type="text"/>
+        </div>
 
         <button className="button button--accent-fill">Get data</button>
     </form>
